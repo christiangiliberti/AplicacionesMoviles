238071b955defb337102a90ed2480f83a4628e3b
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-08-23 19:02:27 -0300

    Update Ejercicio_2.js

--- Ejercicio_2.js
+++ Ejercicio_2.js
@@ -1,2 +1,5 @@
 var Meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-console.log(Meses);
+//console.log(Meses);
+for (let i of Meses){
+    console.log(i);
+}
