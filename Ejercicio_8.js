fbf00fb222ceb5ee14597b54193ed94b581bb5ba
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-08-31 12:25:16 -0300

    Update Ejercicio_8.js

--- Ejercicio_8.js
+++ Ejercicio_8.js
@@ -1,14 +1,15 @@
 var numbers = [7,9,25,42];
 
-var even=function(num){
-var par=0;
-for(var i of num){
+var p=0;
+function even (n){
+  for(var i of n){
     if (i % 2 == 0){
-        par++;
+        p++;
     }
 }
-console.log(par)
+console.log(p);
 }
+
 
 console.log(even(numbers));
 
