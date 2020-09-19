d024bdc03730adf2e078075b50e3cf25874111c3
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-09-19 13:25:54 -0300

    .

--- Ejercicio_10.js
+++ Ejercicio_10.js
@@ -2,9 +2,8 @@ var numbers = [7,8,9];
 
 
 function contiene(a,n){
-    var res = a.some(numero => numero == n);
-    console.log(res);
+    return a.some(numero => numero == n);
+    
 }
-
 
 console.log(contiene(numbers,8));
