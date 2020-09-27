a0b9c0543f556076233224d85d4593cc72e7bd77
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-09-27 10:40:10 -0300

    .

--- Ejercicio_9.js
+++ Ejercicio_9.js
@@ -1,14 +1,11 @@
 var numbers = [7,9,87,42];
 
-
-function even (n){
-    if (n % 2 == 1){
-        return true;
-    }
+function esPar(num){
+    return num %2 === 0
 }
-function rechazar (n,a){
-    return a.filter(n=>even(n));
-    }
 
+function rechazar (n,array){
+    return array.filter(num => n (num))
+}
 
-console.log(rechazar(even,numbers));
+console.log(rechazar(esPar,(numbers)))
